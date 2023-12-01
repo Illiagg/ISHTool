8448a2b783affaea5ee137489ac7649b492e7bee
Illia__ <105129828+Illiagg@users.noreply.github.com>
2023-12-01 22:47:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,47 @@ import socket
 import validators
 import requests
 import threading
-import time
 import phonenumbers
 from phonenumbers import geocoder, carrier, timezone
+from faker import Faker
+import random
+from bs4 import BeautifulSoup
+
+fake = Faker('ru_RU')
+
+def generate_fake_info():
+    fake_name = fake.name()
+    fake_country = fake.country()
+    fake_city = fake.city()
+    fake_address = fake.address()
+    fake_job = fake.job()
+    fake_gender = random.choice(['Мужской', 'Женский'])
+    fake_birthdate = fake.date_of_birth(minimum_age=18, maximum_age=90)
+    fake_email = fake.email()
+    fake_phone_number = fake.phone_number()
+    fake_company = fake.company()
+    fake_ssn = fake.ssn()
+    fake_username = fake.user_name()
+    fake_website = fake.url()
+    fake_text = fake.text()
+    
+    fake_info = {
+        'ФИО': fake_name,
+        'Страна': fake_country,
+        'Город': fake_city,
+        'Адресс': fake_address,
+        'Работа': fake_job,
+        'Пол': fake_gender,
+        'Дата рождения': fake_birthdate.strftime("%Y-%m-%d"),
+        'Электронная почта': fake_email,
+        'Телефон': fake_phone_number,
+        'Компания': fake_company,
+        'SSN': fake_ssn,
+        'Имя пользователя': fake_username,
+        'Веб-сайт': fake_website,
+        'Текст': fake_text
+    }
+    return fake_info
 
 def make_request(url):
     response = requests.get(url)
@@ -35,7 +73,6 @@ def analyze_phone_number(phone_number, numverify_api_key):
 ╚══════════════                     ══════════════╝
 ''')
 
-        # Requesting additional data from NumVerify
         numverify_url = f'http://apilayer.net/api/validate?access_key={numverify_api_key}&number={phone_number}&country_code=&format=1'
         response = requests.get(numverify_url)
         data = response.json()
@@ -82,12 +119,12 @@ def get_ip():
         
     [+] Continent: {infoList["continent"]}
     [+] Country: {infoList["country"]}
+    
     [+] Capital: {infoList["capital"]}
     [+] Region: {infoList["region"]}
     [+] City: {infoList["city"]}
         
     [+] Postal Code: {infoList["postal"]}
-    [+] Capital: {infoList["capital"]}
         
     [+] Location: {infoList["latitude"]} {infoList["longitude"]}
         
@@ -106,6 +143,72 @@ def get_ip():
 ''')
     except Exception as e:
         print(f'Error: {e}')
+       
+def find_social_media_links(username):
+    social_media_links = {}
+    
+    soundcloud_url = f"https://soundcloud.com/{username}"
+    soundcloud_response = requests.get(soundcloud_url)
+    if soundcloud_response.status_code == 200:
+        social_media_links['SoundCloud'] = soundcloud_url
+    
+    myspace_url = f"https://myspace.com/{username}"
+    myspace_response = requests.get(myspace_url)
+    if myspace_response.status_code == 200:
+        social_media_links['MySpace'] = myspace_url
+
+    github_url = f"https://github.com/{username}"
+    github_response = requests.get(github_url)
+    if github_response.status_code == 200:
+        social_media_links['GitHub'] = github_url
+    
+    telegram_url = f"https://t.me/{username}"
+    telegram_response = requests.get(telegram_url)
+    if telegram_response.status_code == 200:
+        social_media_links['Telegram'] = telegram_url
+        
+    tiktok_url = f"https://www.tiktok.com/@{username}"
+    tiktok_response = requests.get(tiktok_url)
+    if tiktok_response.status_code == 200:
+        social_media_links['TikTok'] = tiktok_url
+
+    likee_url = f"https://likee.com/{username}"
+    likee_response = requests.get(likee_url)
+    if likee_response.status_code == 200:
+        social_media_links['Likee'] = likee_url
+
+    youtube_url = f"https://www.youtube.com/user/{username}"
+    youtube_response = requests.get(youtube_url)
+    if youtube_response.status_code == 200:
+        social_media_links['YouTube'] = youtube_url
+        
+    pinterest_url = f"https://www.pinterest.com/{username}"
+    pinterest_response = requests.get(pinterest_url)
+    if pinterest_response.status_code == 200:
+        pinterest_soup = BeautifulSoup(pinterest_response.text, 'html.parser')
+        user_found = pinterest_soup.find('title', string=f"{username} on Pinterest")
+        if user_found:
+            social_media_links['Pinterest'] = pinterest_url
+
+    reddit_url = f"https://www.reddit.com/user/{username}"
+    reddit_response = requests.get(reddit_url)
+    if reddit_response.status_code == 200:
+        reddit_soup = BeautifulSoup(reddit_response.text, 'html.parser')
+        user_found = reddit_soup.find('h1', {'class': 'ListingLayout-children'})
+        if user_found:
+            social_media_links['Reddit'] = reddit_url
+    
+    instagram_url = f"https://www.instagram.com/{username}"
+    instagram_response = requests.get(instagram_url)
+    if instagram_response.status_code == 200:
+        social_media_links['Instagram'] = instagram_url
+        
+    snapchat_url = f"https://www.snapchat.com/add/{username}"
+    snapchat_response = requests.get(snapchat_url)
+    if snapchat_response.status_code == 200:
+        social_media_links['Snapchat'] = snapchat_url
+    
+    return social_media_links
         
 import os
 from pystyle import Colorate, Colors
@@ -138,7 +241,7 @@ while True:
         analyze_phone_number(phone_input, numverify_api_key)
         input("[-] Press ENTER, to continue")
     elif select == "3":
-        url = input("[+] Type site's URL:")
+        url = input("[+] Type site's URL: ")
         if not validators.url(url):
             print("[!] Incorrect URL!")
         else:
@@ -147,6 +250,21 @@ while True:
                 t = threading.Thread(target=make_request, args=(url,))
                 t.start()
     elif select == "4":
+        fake_data = generate_fake_info()
+        for info, value in fake_data.items():
+            print(f'[+] {info}: {value}')
+        input("[-] Press ENTER, to continue")
+    elif select == "5":
+        username = input("[+] Search in:\nYouTube, TikTok, Likee, GitHub, Reddit, Telegram, Pinterest, Snapchat, SoundCloud, MySpace, Instagram(bad)\n[+] Type username (without @): ")
+        social_media_links = find_social_media_links(username)
+        if social_media_links:
+            print("[+] Possible socials:")
+            for platform, link in social_media_links.items():
+                print(f"{platform}: {link}")
+        else:
+            print("[!] Nothing found")
+        input("[-] Press ENTER, to continue")
+    elif select == "6":
         print('\nby @illiagg3 <- banned')
         print('github: https://github.com/Illiagg')
         print('version: 0.1')
